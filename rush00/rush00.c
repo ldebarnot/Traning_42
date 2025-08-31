877b1eebb6e0f47d5b8960c7f80d95e42af241a4
Lucas Debarnot <lucasdebarnot@MacBook-Air-de-Lucas.local>
2025-08-31 08:51:32 +0200

    test pr rush01

--- rush00/rush00.c
+++ rush00/rush00.c
@@ -1,14 +1,72 @@
-void ft_putchar (char c)
+#include <unistd.h>
+//ligne x
+//col y
+int put_box(int **tab, int x, int y)
 {
-    write (1, &c, 1);
+    int *box = 1;
+        while (y < 6)
+        if (tab[x][y] == 4)
+        {
+            box = 4;
+            return(box);
+        }
+        else if (tab[x][y] == 1)
+        {
+            while (box < 5)
+            {
+                return(box);
+                box++;
+            }
+        }
 }
-void    print_top(char l)
+int possible_box4(int **tab, int x, int y)
 {
-    ft_putchar('A');
-    while (l > 2)
+    int *box;
+    if (tab[x] == 4)
+        {
+            box = {4, 3, 2 , 1};
+            box = {4, 3 , 1, 2};
+            box = {4, 2, 3, 1};
+            box = {4, 2, 1, 3};
+            box = {4, 1, 2, 3};
+            box = {4, 1, 3, 2};
+            return (box);
+        }
+        return (0)
+}
+int possible_box3(int **tab, int x, int y)
+{
+    int *box;
+    if (tab[x] == 3)
     {
-        ft_putchar('B');
-        l--;
+        box = {1, 2, 4, 3};
+        box = {1, 3, 4, 2};
+        box = {1, 3, 2, 4};
+        box = {2, 3, 4, 1};
+        box = {2, 1, 3, 4};
+        box = {2, 3, 1, 4};
+        return (box);
     }
-    ft_putchar ('A');
+    return (0);
+}
+int possible_box2(int **tab, int x, int y)
+{
+    int *box;
+    if (tab[x] == 2)
+    {
+        box = {1, 4, 3, 2};
+        box = {1, 4, 2, 3};
+        box = {2, 4, 3, 1};
+        box = {2, 4, 1, 3};
+        box = {2, 1, 4, 3};
+        box = {3, 2, 1, 4};
+        box = {3, 1, 2, 4};
+        box = {3, 2, 4, 1};
+        box = {3, 1, 4, 2};
+        box = {3, 4, 1, 2};
+        box = {3, 4, 2, 1};
+    }
+    else if (box[x] == 1)
+        box = {1, 2, 3, 4};
+    return (box);
 }
